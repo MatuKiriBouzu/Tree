0829271059384805426dffcd6f489d63b8d18240
gussan4125 <gussan11ksu11wo@gmail.com>
2013-06-10 23:04:58 +0900

    出口

--- TreeProject/TreeProject/TreeModel.java
+++ TreeProject/TreeProject/TreeModel.java
@@ -38,71 +38,70 @@ public class TreeModel
 	private BufferedReader in;
 	
 	/**
-	 *getter
-	 *NodesMaxの値を返す
-	 *松きり坊主 144542 2013/6/3
+	 * getter
+	 * NodesMaxの値を返す
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public int getNodesMax()
 	{
 		return 0;
 	}
 	/**
-	 *setter
-	 *NodesMaxを書き換える
-	 *松きり坊主 144542 2013/6/3
+	 * setter
+	 * NodesMaxを書き換える
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public void setNodesMax(int x)
 	{
 		return;
 	}
 	/**
- 	 *getter
-	 *BranchsMaxの値を返す。
-	 *松きり坊主 144542 2013/6/3
-	 *
+ 	 * getter
+	 * BranchsMaxの値を返す。
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public int getBranchsMax()
 	{
 		return 0;
 	}
 	/**
-	 *setter
-	 *BranchsMaxの値を書き替える
-	 *松きり坊主 144542 2013/6/3
+	 * setter
+	 * BranchsMaxの値を書き替える
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public void setBranchsMax()
 	{
 		return;
 	}
 	/**
-	 *getter
-	 *Nodesのindex番目のnodeを返す
-	 *松きり坊主 144542 2013/6/3
+	 * getter
+	 * Nodesのindex番目のnodeを返す
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public TreeNode getNodes(int index)
 	{
 		return null;
 	}
 	/**
-	 *TreeNode.TreeBranchのPoint情報からそれぞれの場所を計算する。
-	 *松きり坊主 144542 2013/6/3
+	 * TreeNode.TreeBranchのPoint情報からそれぞれの場所を計算する。
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public void calculateTree()
 	{
 		return;
 	}
 	/**
-	 *TreeNode.TreeBranchのPoint情報からそれぞれの場所を計算する様子を
-	 *アニメーションにする
-	 *松きり坊主 144542 2013/6/3
+	 * TreeNode.TreeBranchのPoint情報からそれぞれの場所を計算する様子を
+	 * アニメーションにする
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public void animationTree()
 	{
 		return;
 	}
 	/**
-	 *ファイルからそれぞれの情報を読み取る
-	 *松きり坊主 144542 2013/6/3
+	 * ファイルからそれぞれの情報を読み取る
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public void inputTree(String fileName)
 	{
@@ -128,32 +127,32 @@ public class TreeModel
 		return;
 	}
 	/**
-	 *Nodeの情報をファイルから読む
-	 *松きり坊主 144542 2013/6/3
+	 * Nodeの情報をファイルから読む
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public TreeNode inputNode()
 	{
 		return null;
 	}
 	/**
-	 *Branchの情報をファイルから読む
-	 *松きり坊主 144542 2013/6/3
+	 * Branchの情報をファイルから読む
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public TreeBranch inputBranch()
 	{
 		return null;
 	}
 	/**
-	 *View,Controllerに報告する.
-	 *松きり坊主 144542 2013/6/3
+	 * View,Controllerに報告する.
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public void change()
 	{
 		return;
 	}
 	/**
-	 *描画する。
-	 *松きり坊主 144542 2013/6/3
+	 * 描画する。
+	 * 松きり坊主 144542 2013/6/3
 	 **/
 	public void picture()
 	{
