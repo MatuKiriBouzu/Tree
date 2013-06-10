568590c17f5cf0ce0e084f15933bf89cf3efbfaa
gussan4125 <gussan11ksu11wo@gmail.com>
2013-06-10 14:47:12 +0900

    出口 タブなどの書式の修正

--- TreeProject/TreeProject/Example.java
+++ TreeProject/TreeProject/Example.java
@@ -1,5 +1,12 @@
 package TreeProject;
 
+/**
+ * 読み込んだテキストファイルから樹状図を作成するアプリケーション。
+ * mvcモデルを採用し、グラフィカルに、素早く樹状図を表示する。
+ * 他のアプリケーションと比較した際のこのアプリケーションの独自性は、[今の所なさそうなのでなにか考える]
+ * DeguchiShin 144849 6/10 概要記述
+ **/
+
 public class Example {
 
 	public static void main(String[] args)
