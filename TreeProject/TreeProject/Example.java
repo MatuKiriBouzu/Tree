fd7ff8d7533bda43bb09f50723e2d818aa5e9978
MatuKiriBouzu <kym050227@yahoo.co.jp>
2013-06-11 13:36:47 +0900

    Exsample改変

--- TreeProject/TreeProject/Example.java
+++ TreeProject/TreeProject/Example.java
@@ -1,16 +1,14 @@
 package TreeProject;
 
-/**
- * 読み込んだテキストファイルから樹状図を作成するアプリケーション。
- * mvcモデルを採用し、グラフィカルに、素早く樹状図を表示する。
- * 他のアプリケーションと比較した際のこのアプリケーションの独自性は、[今の所なさそうなのでなにか考える]
- * DeguchiShin 144849 6/10 概要記述
- **/
-
 public class Example {
 
 	public static void main(String[] args)
 	{
-		System.out.println("開始");
+		TreeModel a = new TreeModel();
+		TreeBranch k = null;
+		a.inputTree("aaaa");
+		k = (TreeBranch)a.branchs.get(1);
+		System.out.println(k.parent);
+		
 	}
 }
